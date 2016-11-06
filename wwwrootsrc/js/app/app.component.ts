6c28d70e8b633cfe28274013e2322d2f21c1a5aa
Eric W. Greene <eric@ericwgreene.com>
2016-11-06 07:40:10 -0500

    Cleaned up project for class.

--- wwwrootsrc/js/app/app.component.ts
+++ wwwrootsrc/js/app/app.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
-import { AppStorage } from './services/app-storage';
-
+	
 @Component({
 	selector: 'main',
 	styles: [require('./app.component.scss')],
 	template: require('./app.component.html'),
-	exportAs: 'AppComponent'
 })
 export class AppComponent { }
--- wwwrootsrc/js/app/components/page-header/page-header.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-	selector: 'main > header',
-	template: require('./page-header.component.html'),
-	styles: [require('./page-header.component.scss')]
-})
-export class PageHeaderComponent { }
