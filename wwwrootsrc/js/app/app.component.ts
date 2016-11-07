08d2b277500200872c0b72bb8c86db2d8264049e
Eric W. Greene <eric@ericwgreene.com>
2016-11-07 15:05:58 -0600

    added code demos

--- wwwrootsrc/js/app/app.component.ts
+++ wwwrootsrc/js/app/app.component.ts
@@ -1,8 +1,44 @@
 import { Component } from '@angular/core';
-	
+
 @Component({
 	selector: 'main',
-	styles: [require('./app.component.scss')],
-	template: require('./app.component.html'),
+	template: `<h1>{{header}}</h1>
+	<ul>
+		<li *ngFor="let color of sortedColors">{{color}}</li>
+	</ul>
+	<div>
+		<label for="new-color">New Color</label>
+		<input type="text" id="new-color" [(ngModel)]="newColor">
+		<br><button type="button" (click)="addColor()">Add Color</button>
+	</div>`
 })
-export class AppComponent { }
+export class AppComponent {
+
+	newColor: string = '';
+
+	addColor() {
+		this.colors = this.colors.concat(this.newColor);
+		this.newColor = '';
+	}
+
+	get sortedColors() {
+
+		console.log('sorted colors');
+
+		if (this.lastColors !== this.colors) {
+			console.log('sorting...');
+			this.lastColors = this.colors;
+			return this.colors.sort();
+		}
+
+		return this.colors;
+	}
+
+	header: string = 'List Of Colors';
+
+	colors: string[] = [
+		'red','blue','white','yellow','saffron','green'
+	];
+
+	lastColors: string[] = null;
+}
--- wwwrootsrc/js/app/app.module.ts
+++ wwwrootsrc/js/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import '../../css/styles.scss';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-	imports: [ BrowserModule ],
+	imports: [ BrowserModule, FormsModule ],
 	declarations: [ AppComponent ],
 	bootstrap: [ AppComponent ]
 })
