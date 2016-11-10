010058ca8b6402f0c48a2fa5911e8cec34e25d9c
Eric W. Greene <eric@ericwgreene.com>
2016-11-10 06:38:40 -0600

    added additional form demo content

--- wwwrootsrc/js/app/app.module.ts
+++ wwwrootsrc/js/app/app.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+//import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { UtilsModule } from '../utils/utils.module';
 
 import '../../css/styles.scss';
 
-import { AppComponent, EditRowComponent, RegisterFormModelDirective } from './app.component';
+import { AppComponent } from './app.component';
 
 @NgModule({
-	imports: [ BrowserModule, FormsModule, UtilsModule ],
-	declarations: [ AppComponent, EditRowComponent, RegisterFormModelDirective ],
+	imports: [ BrowserModule, ReactiveFormsModule, UtilsModule ],
+	declarations: [ AppComponent ],
 	bootstrap: [ AppComponent ]
 })
 export class AppModule { }
